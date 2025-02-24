17e6da475ad441f6aa6a70df6a6699aeb8096361
Henry Wong <hlwong.enrique@gmail.com>
2025-02-24 20:38:40 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from function import get_todos, write_todos
+
+import time
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+user_prompt = "Type add, show, edit, complete, or exit: "
+
+
+while True:
+    user_action = input(user_prompt)
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + "\n")
+
+        write_todos(todos)
+
+    elif user_action.startswith("show"):
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip("\n")
+            row = f"{index+1}--{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter new to_do: ")
+            todos[number] = new_todo + "\n"
+
+            write_todos(todos)
+
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos()
+
+            todos.pop(number-1)
+
+            write_todos("todos.txt", todos)
+
+        except IndexError:
+            print("There is no item with that number")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+
+    else:
+        print("something else")
+
+print("Bye! ")
